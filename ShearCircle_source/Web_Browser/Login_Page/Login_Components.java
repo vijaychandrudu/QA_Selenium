bffaf59e115587b2fcb2bd54be39b3b63d6b18b1
pc <pc@192.168.2.18>
2017-10-14 00:06:58 +0530

    Updated

--- ShearCircle_source/Web_Browser/Login_Page/Login_Components.java
+++ ShearCircle_source/Web_Browser/Login_Page/Login_Components.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import TestNG_Driver.Test_Driver;
 import Web_Browser.Web_Browser;
-import Web_Browser.TestNG_Driver.Test_Driver;
 import Web_Browser.browserLaunch.browserlaunch;
 
 public class Login_Components extends browserlaunch{
@@ -19,20 +19,20 @@ public Login_Components(WebDriver getdriver) {
 
 Web_Browser browser = new Web_Browser();
 
-public void customer_login(){
+public void customer_login(String p_in_UserName, String p_in_Password){
 	
-	String p_in_UserName = "test";    
-    String p_in_Password = "test";  
+	//String p_in_UserName = "test";    
+    //String p_in_Password = "test";  
     
-    getdriver.findElement(By.xpath("//tr[2]/td[2]/input[@name='userName']")).sendKeys("kumar");
+    //getdriver.findElement(By.xpath("//tr[2]/td[2]/input[@name='userName']")).sendKeys("kumar");
     
     
  
     
-    /*browser.click(Login.Login_Link);    
+	browser.click(Login.Login_Link);    
     browser.sendkeys(Login.Login_UserName_textbox, p_in_UserName);
     browser.sendkeys(Login.Login_PassWord_textbox, p_in_Password);
-    browser.click(Login.Login_signin_button);*/     
+    browser.click(Login.Login_signin_button);     
   
 
 }
