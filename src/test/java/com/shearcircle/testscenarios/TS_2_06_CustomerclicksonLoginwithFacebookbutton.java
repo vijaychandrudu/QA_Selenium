cf135ead9e8c7dafba07ce07eec1face99348d23
Vijay <Vijay@192.168.2.24>
2017-11-17 23:09:26 +0530

    updated scenario

--- src/test/java/com/shearcircle/testscenarios/TS_2_06_CustomerclicksonLoginwithFacebookbutton.java
+++ src/test/java/com/shearcircle/testscenarios/TS_2_06_CustomerclicksonLoginwithFacebookbutton.java
@@ -53,12 +53,12 @@ public class TS_2_06_CustomerclicksonLoginwithFacebookbutton extends StaticVaria
 		CR.ShearCircle_LaunchApplication();			
 	}
 	
-	@Test(priority=0, description="TC_002 Customer clicks Login on ShearCircle home page " )
+	@Test(priority=1, description="TC_002 Customer clicks Login on ShearCircle home page " )
 	public void TC_002_Customer_clicks_JoinOurCircleButton() throws IOException {			
 		CR.ShearCircle_Click_Login_OR_JoinOurCircle("Click_Login");			
 	}
 	
-	@Test(priority=0, description="TC_003 Customer clicks on Login with Facebook button" )
+	@Test(priority=2, description="TC_003 Customer clicks on Login with Facebook button" )
 	public void TC_003_CustomerclicksonLoginwithFacebookbutton() throws IOException {			
 		CR.Click_DoNotHaveAccountjoinourcircle_Loginwith_FaceBook_Google_Bottons("Click_LoginwithFaceBook");		
 	}	
