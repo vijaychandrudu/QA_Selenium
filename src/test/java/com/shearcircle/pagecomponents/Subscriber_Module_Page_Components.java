5111e345ed9faea1bd7b6a4e0d1f8282deb24b6a
vijay <vijay@192.168.2.4>
2017-11-18 11:34:14 +0530

    Updated Common method name

--- src/test/java/com/shearcircle/pagecomponents/Subscriber_Module_Page_Components.java
+++ src/test/java/com/shearcircle/pagecomponents/Subscriber_Module_Page_Components.java
@@ -41,7 +41,7 @@ public class Subscriber_Module_Page_Components extends StaticVariables {
 				System.out.println("Browser Tilte:" + apptitle);
 			}
 
-			if (browser.elmentisdisplayed(Login.Home_ShearCircle_Image)) {
+			if (browser.elementisdisplayed(Login.Home_ShearCircle_Image)) {
 				System.out.println("ShearCircle Image displayed");
 			} else {
 				System.out.println("ShearCircle Image not displayed");
@@ -68,30 +68,30 @@ public void Subscriber_Registration(){
 		Subdomain = browser.getdata("Subdomain");
 		try {	
 		
-		if(browser.elmentisdisplayed(Login.Home_JoinOurCircle_Link)) {
+		if(browser.elementisdisplayed(Login.Home_JoinOurCircle_Link)) {
 			browser.reportscomtep("Passed", "Verify JoinCircle link is displayed"  , "JoinCircle link should be displayed", "JoinCircle link is displayed");
 			browser.click(Login.Home_JoinOurCircle_Link);
 			//browser.wait(5);
 	
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_FirstName_Textbox, "FirstName textbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_FirstName_Textbox, "FirstName textbox");
 					browser.sendkeys(SCobjects.Subscriber_FirstName_Textbox,FirstName);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_LastName_Textbox, "LastName textbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_LastName_Textbox, "LastName textbox");
 					browser.sendkeys(SCobjects.Subscriber_LastName_Textbox,LastName);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_Email_Textbox, "Email textbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_Email_Textbox, "Email textbox");
 					browser.sendkeys(SCobjects.Subscriber_Email_Textbox,ProfessionalEmail);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_BusinessName_Textbox, "buisiness textbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_BusinessName_Textbox, "buisiness textbox");
 					browser.sendkeys(SCobjects.Subscriber_BusinessName_Textbox,BusinessName);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_Subdomain_textbox, "Subdomain textbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_Subdomain_textbox, "Subdomain textbox");
 					browser.sendkeys(SCobjects.Subscriber_Subdomain_textbox,Subdomain);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_Password_textbox, "Password textbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_Password_textbox, "Password textbox");
 					browser.sendkeys(SCobjects.Subscriber_Password_textbox,Password);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_ReEnterPassword_textbox, "ReEnteredPassword textbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_ReEnterPassword_textbox, "ReEnteredPassword textbox");
 					browser.sendkeys(SCobjects.Subscriber_ReEnterPassword_textbox,Password);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_Termsofservice_checkbox, "Termsofservice checkbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_Termsofservice_checkbox, "Termsofservice checkbox");
 					browser.check_Checkbox(SCobjects.Subscriber_Termsofservice_checkbox);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_billingagrement_checkbox, "billingagrement Checkbox");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_billingagrement_checkbox, "billingagrement Checkbox");
 					browser.check_Checkbox(SCobjects.Subscriber_billingagrement_checkbox);
-					browser.Verify_elmentisdisplayed_Report(SCobjects.Subscriber_joinourcircle_button, "JoinCircle Button");
+					browser.Verify_elementisdisplayed_Report(SCobjects.Subscriber_joinourcircle_button, "JoinCircle Button");
 					browser.click(SCobjects.Subscriber_joinourcircle_button);
 				}
 			
